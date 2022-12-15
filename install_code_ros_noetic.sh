1fbeb315c13ae0ff7a9ec692ff713f6b9e3a09f4
tatwik-yenikapati <110340075+tatwik-yenikapati@users.noreply.github.com>
2022-12-15 20:46:47 +0530

    Create install_code_ros_noetic.sh
    A bash file to install curl ,VS Code, and ROS noetic full

---
+++ install_code_ros_noetic.sh
@@ -0,0 +1,35 @@
+sudo apt update
+### curl ###
+
+sudo apt install curl
+
+### VS Code ###
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get update
+sudo apt-get install -y code
+
+##### ROS ####
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update
+
+sudo apt install ros-noetic-desktop-full
+
+source /opt/ros/noetic/setup.bash
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+
+sudo apt install python3-rosdep
+
+sudo rosdep init
+
+rosdep update
